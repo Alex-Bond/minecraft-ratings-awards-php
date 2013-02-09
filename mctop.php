f8c2b5138a67f16e1f01bc4e5234cc23d1fbe0c6
Alex Bond <alex.bondarevskiy@gmail.com>
2013-02-09 03:23:37 +0200

    Небольшое изменение структуры конфига чтобы было понятнее.

--- mctop.php
+++ mctop.php
@@ -7,34 +7,34 @@ $config = array(
         'username' => 'root',
         'password' => '',
         'database' => 'minecraft',
-        'tables' => array(
-            array(
-                'active' => true,
-                'table-name' => 'iConomy',
-                'type' => 0,
-                /*
-                 * # ДОБАВЛЕНИЕ К ЧИСЛЕННОМУ ЗНАЧЕНИЮ
-                 * 0 - только обновление (пополнение).
-                 * 1 - вставка нового параметра если нет такой записи (поле user-column дожно быть уникально в схеме (уникальный индекс)).
-                 * # ВЫСТАВЛЕНИЕ ЗНАЧЕНИЯ
-                 * 2 - выставления значения
-                 * 3 - вставка или выставление значения (поле user-column дожно быть уникально в схеме (уникальный индекс)).
-                 * */
-                'change-column' => 'balance',
-                'user-column' => 'username',
-                'default' => 30, // Стандартный баланс
-                'change' => 1 // Бонус или значение
-            )
-        )
     ),
     'security-code' => '',
+    'jobs' => array(
+        array(
+            'active' => true,
+            'table-name' => 'iConomy',
+            'type' => 0,
+            /*
+             * # ДОБАВЛЕНИЕ К ЧИСЛЕННОМУ ЗНАЧЕНИЮ
+             * 0 - только обновление (пополнение).
+             * 1 - вставка нового параметра если нет такой записи (поле user-column дожно быть уникально в схеме (уникальный индекс)).
+             * # ВЫСТАВЛЕНИЕ ЗНАЧЕНИЯ
+             * 2 - выставления значения
+             * 3 - вставка или выставление значения (поле user-column дожно быть уникально в схеме (уникальный индекс)).
+             * */
+            'change-column' => 'balance',
+            'user-column' => 'username',
+            'default' => 30, // Стандартный баланс
+            'change' => 1 // Бонус или значение
+        )
+    )
 );
 
 if (isset($_GET['hash']) && isset($_GET['player']) && strlen($_GET['hash']) == 32 && strlen($_GET['player']) >= 3 && $_GET['hash'] == md5($config['security-code'] . $_GET['player'])) {
     try {
         $db = new PDO($config['db']['driver'] . ':host=' . $config['db']['host'] . ';dbname=' . $config['db']['database'], $config['db']['username'], $config['db']['password']);
 
-        foreach ($config['db']['tables'] as $key => $item) {
+        foreach ($config['jobs'] as $item) {
             if ($item['active'] == true) {
                 switch ($item['type']) {
                     case 0:
